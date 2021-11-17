312dd9968f30970cd1c1c5b87bd257dac52c0f14
VladNCode <vldsmrfow@gmail.com>
2021-11-17 14:07:19 +0200

    increased output delay

--- public/js/app.js
+++ public/js/app.js
@@ -6,7 +6,7 @@ const input = document.querySelector('.imp');
 const weatherData = document.querySelector('.weather--data');
 
 let timer;
-const doneTypingInterval = 1500; // wait 1.5 seconds
+const doneTypingInterval = 2500; // wait 1.5 seconds
 
 if (form) {
   form.addEventListener('submit', e => {
@@ -57,37 +57,3 @@ if (form) {
     form.reset();
   }
 }
-
-// if (form)
-//   form.addEventListener('submit', e => {
-//     e.preventDefault();
-
-//     input.placeholder = 'Loading...';
-
-//     fetch(`https://127.0.0.1:3000/api?adress=${input.value}`)
-//       .then(response => {
-//         return response.json();
-//       })
-//       .then(data => {
-//         console.log(data);
-
-//         if (data.status === 'fail') {
-//           input.placeholder = 'Loading...';
-//           weatherData.innerHTML = `
-//         <h3>Status: "${data.status}"</h3>
-//         <h3>${data.message}</h3>`;
-//           input.placeholder = '';
-//           return input.blur();
-//         }
-
-//         weatherData.innerHTML = `
-//       <br>
-//       <img src="${data.icon}" alt="weather icon">
-//       <h3>${data.location}</h3>
-//       <h3>${data.data}</h3>`;
-//         input.placeholder = '';
-//         input.blur();
-//       });
-
-//     form.reset();
-//   });
