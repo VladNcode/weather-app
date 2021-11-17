efe2537b9826612198c1c18744167466962735db
VladNCode <vldsmrfow@gmail.com>
2021-11-17 10:37:38 +0200

    added front-end success and error appends

--- public/js/app.js
+++ public/js/app.js
@@ -1,9 +1,9 @@
 console.log('Client side js file is loaded');
 
-const wrapper = document.querySelector('.weather--fe');
-const form = document.querySelector('.weather--data');
+const wrapper = document.querySelector('.weather--container');
+const form = document.querySelector('.weather--form');
 const input = document.querySelector('.imp');
-// const btn = document.querySelector('.imp-btn');
+const weatherData = document.querySelector('.weather--data');
 
 if (form)
   form.addEventListener('submit', e => {
@@ -15,9 +15,18 @@ if (form)
       })
       .then(data => {
         console.log(data);
-        wrapper.innerHTML += `
+
+        if (data.error)
+          return (weatherData.innerHTML = `
+        <h3>${data.error.statusCode}</h3>
+        <h3>${data.message}</h3>`);
+
+        weatherData.innerHTML = `
+        <br>
         <img src="${data.icon}" alt="weather icon">
         <h3>${data.location}</h3>
         <h3>${data.data}</h3>`;
       });
+
+    form.reset();
   });
